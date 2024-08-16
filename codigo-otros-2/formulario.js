3b22aa848a9603a03acdd0ba7eb4ddc63c6a26e0
ErickERC <erguez.eerc@gmail.com>
2024-08-16 05:33:07 -0600

    (+) Todo referente a revisión de código de otros 2

---
+++ codigo-otros-2/formulario.js
@@ -0,0 +1,75 @@
+const formulario = document.querySelector(".formulario");
+
+formulario.onsubmit = function (event) {
+  event.preventDefault();
+
+  let n = formulario.elements[0];
+  let e = formulario.elements[1];
+  let na = formulario.elements[2];
+
+  let nombre = n.value;
+  let edad = e.value;
+
+  let i = na.selectedIndex;
+  let nacionalidad = na.options[i].value;
+  console.log(nombre, edad);
+  console.log(nacionalidad);
+
+  if (nombre.length === 0) {
+    n.classList.add(".error");
+  }
+  if (edad < 18 || edad > 120) {
+    e.classList.add(".error");
+  }
+
+  if (nombre.length > 0 && edad > 18 && edad < 120) {
+    agregarInvitado(nombre, edad, nacionalidad);
+  }
+};
+
+function agregarInvitado(nombre, edad, nacionalidad) {
+  if (nacionalidad === "ar") {
+    nacionalidad = "Argentina";
+  } else if (nacionalidad === "mx") {
+    nacionalidad = "Mexicana";
+  } else if (nacionalidad === "vnzl") {
+    nacionalidad = "Venezolana";
+  } else if (nacionalidad === "per") {
+    nacionalidad = "Peruana";
+  }
+
+  let lista = document.getElementById("lista-de-invitados");
+
+  let elementoLista = document.createElement("div");
+  elementoLista.classList.add("elemento-lista");
+  lista.appendChild(elementoLista);
+
+  function crearElemento(descripcion, valor) {
+    var spanNombre = document.createElement("span");
+    var inputNombre = document.createElement("input");
+    var espacio = document.createElement("br");
+    spanNombre.textContent = descripcion + ": ";
+    inputNombre.value = valor;
+    elementoLista.appendChild(spanNombre);
+    elementoLista.appendChild(inputNombre);
+    elementoLista.appendChild(espacio);
+  }
+
+  crearElemento("Nombre", nombre);
+  crearElemento("Edad", edad);
+  crearElemento("Nacionalidad", nacionalidad);
+
+  let botonBorrar = document.createElement("button");
+  botonBorrar.textContent = "Eliminar invitado";
+  botonBorrar.id = "boton-borrar";
+  let corteLinea = document.createElement("br");
+  document.body.appendChild(corteLinea);
+  document.body.appendChild(botonBorrar);
+
+  elementoLista.appendChild(botonBorrar);
+
+  botonBorrar.onclick = function () {
+    // this.parentNode.style.display = 'none';
+    botonBorrar.parentNode.remove();
+  };
+}
